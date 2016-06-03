a573f4258950f6e48fb2117bd90c27be5fea6998
wheatwizard <olivee3@rpi.edu>
2016-06-02 20:19:01 -0400

    Made new plot class

---
+++ plot.py
@@ -0,0 +1,88 @@
+class Plot(object):
+	def __init__(self, x_min, x_max, x_size, y_min, y_max, y_size):
+		self.x_min = x_min
+		self.x_max = x_max
+		self.x_size = x_size
+		self.y_min = y_min
+		self.y_max = y_max
+		self.y_size = y_size
+		self.internal_plot = [[' ' for y in range(y_min, y_max+1)] for x in range(x_min, x_max+1)]
+	def set(self,a,b,char):
+		if (a < self.x_max and a > self.x_min and b < self.y_max and b > self.y_min):
+			self.internal_plot[a][b] = char
+	def plot_axes(self):
+		for x in range(x_min, x_max):
+			plot[x][0] = '.'
+		for y in range(y_min, y_max):
+			plot[0][y] = ':'
+	def print_plot(self):
+		result = ''
+		for y in range(self.y_min, len(self.internal_plot[0])+self.y_min)[::-1]:
+			for x in range(self.x_min, len(self.internal_plot)+self.x_min):
+				result += self.internal_plot[x][y]
+			result += '\n'
+		print result
+	def plot_function(self, function):
+		
+		#Modify the function
+		def f(x):
+			try:
+				return function(x*self.x_size)/self.y_size
+			except:	
+				#If x is not in the domain of the function plot it outside of the graph
+				return self.y_max+10
+		
+		for x in range(self.x_min, self.x_max):
+			back_diff = f(x)-f(x-1)
+						
+			#Check that the last value was valid
+			try:
+				function((x-1)*self.x_size)/self.y_size
+			except:
+				back_diff = 0
+			
+			diff = f(x+.5)-f(x-.5)
+			front_diff = f(x+1)-f(x)	
+			
+			#Check that the next value was valid
+			try:
+				function((x+1)*self.x_size)/self.y_size
+			except:
+				front_diff = 0
+	
+			if abs(diff) < .5:
+				floor_diff = f(x)-int(f(x))
+				if floor_diff > .5:
+					self.set(x,int(f(x))+1,'_')
+				elif floor_diff < -.5:
+					self.set(x,int(f(x)),'_')				
+				else:
+					self.set(x,int(f(x)),'-')
+			elif abs(diff) > 2:
+				self.set(x,int(f(x)),'|')
+			elif diff < 0:
+				self.set(x,int(f(x)),'\\')
+			elif diff > 0:
+				self.set(x,int(f(x)),'/')
+			
+			#Complete the line if it is non-continuous
+			if back_diff < -1:
+				y = int(f(x))+1
+				while y < f(x-.5) and y < self.y_max:
+					self.set(x,y,'|')
+					y += 1
+			elif back_diff > 1:
+				y = int(f(x))-1
+				while y > f(x-.5) and y > self.y_min:
+					self.set(x,y,'|')
+					y -= 1
+			if front_diff < -1:
+				y = int(f(x))-1
+				while y > f(x+.5) and y > self.y_min:
+					self.set(x,y,'|')
+					y -= 1
+			elif front_diff > 1:
+				y = int(f(x))+1
+				while y < f(x+.5) and y < self.y_max:
+					self.set(x,y,'|')
+					y += 1	
--- plotter.py
+++ plotter.py
@@ -2,19 +2,13 @@ import math
 
 from sys import argv
 
+from plot import Plot
+	
 def abs(x):
 	if x < 0:
 		return -x
 	return x
-
-def print_plot(plot, x_min, y_min):
-	result = ''
-	for y in range(y_min, len(plot[0])+y_min)[::-1]:
-		for x in range(x_min, len(plot)+x_min):
-			result += plot[x][y]
-		result += '\n'
-	print result
-
+	
 if __name__ == '__main__':
 	if len(argv) < 2:
 		print "Please provide a file name for parameters"
@@ -25,81 +19,11 @@ if __name__ == '__main__':
 	#Execute the code in the test file
 	exec(open(file_name).read())
 	
-	#Modify the function
-	def f(x):
-		try:
-			return function(x*x_size)/y_size
-		except:	
-			#If x is not in the domain of the function plot it outside of the graph
-			#(This causes some issues with slope to be fixed later)
-			return y_max+10
-	
-	plot = [[' ' for y in range(y_min, y_max+1)] for x in range(x_min, x_max+1)]
-	
+	plot = Plot(x_min, x_max, x_size, y_min, y_max, y_size)
+		
 	#Plot axes
-	if plot_axes:
-		for x in range(x_min, x_max):
-			plot[x][0] = '.'
-		for y in range(y_min, y_max):
-			plot[0][y] = ':'
+	if plot_axes: plot.plot_axes
 	
 	#Plot function
-	for x in range(x_min, x_max):
-		if int(f(x)) > y_max or int(f(x)) < y_min:
-			continue
-		back_diff = f(x)-f(x-1)
-		
-		#Check that the last value was valid
-		try:
-			function((x-1)*x_size)/y_size
-		except:
-			back_diff = 0
-		
-		diff = f(x+.5)-f(x-.5)
-		front_diff = f(x+1)-f(x)	
-		
-		#Check that the next value was valid
-		try:
-			function((x+1)*x_size)/y_size
-		except:
-			front_diff = 0
-				
-
-		if abs(diff) < .5:
-			floor_diff = f(x)-int(f(x))
-			if floor_diff > .5 and int(f(x))+1 < y_max:
-				plot[x][int(f(x))+1] = '_'
-			elif floor_diff < -.5:
-				plot[x][int(f(x))] = '_'				
-			else:
-				plot[x][int(f(x))] = '-'
-		elif abs(diff) > 2:
-			plot[x][int(f(x))] = '|'
-		elif diff < 0:
-			plot[x][int(f(x))] = '\\'
-		elif diff > 0:
-			plot[x][int(f(x))] = '/'
-		
-		#Complete the line if it is non-continuous
-		if back_diff < -1:
-			y = int(f(x))+1
-			while y < f(x-.5) and y < y_max:
-				plot[x][y] = '|'
-				y += 1
-		elif back_diff > 1:
-			y = int(f(x))-1
-			while y > f(x-.5) and y > y_min:
-				plot[x][y] = '|'
-				y -= 1
-		if front_diff < -1:
-			y = int(f(x))-1
-			while y > f(x+.5) and y > y_min:
-				plot[x][y] = '|'
-				y -= 1
-		elif front_diff > 1:
-			y = int(f(x))+1
-			while y < f(x+.5) and y < y_max:
-				plot[x][y] = '|'
-				y += 1	
-	print_plot(plot, x_min, y_min)
-	
+	plot.plot_function(function)
+	plot.print_plot()	
