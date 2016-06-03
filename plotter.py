6a53a30e85f869fc7c504030049ac0c79176c7f1
wheatwizard <olivee3@rpi.edu>
2016-06-03 16:34:45 -0400

    Added a auto fitting feature

--- plotter.py
+++ plotter.py
@@ -19,6 +19,21 @@ if __name__ == '__main__':
 	#Execute the code in the test file
 	exec(open(file_name).read())
 	
+	#Determine Autofit bounds
+	if autofit:
+		y_max = 0
+		y_min = 0
+		for function in functions:
+			#define temporary function to prevent domain errors
+			def f(x):
+				try: return function(x*x_size)/y_size
+				except:	return 0
+			values = [ f(x) for x in range(x_min, x_max) ]
+			y_max = max(y_max,max(values))
+			y_min = min(y_min,min(values))
+		y_max = int(y_max + 1) + 1
+		y_min = int(y_min - 1) - 1		
+	
 	plot = Plot(x_min, x_max, x_size, y_min, y_max, y_size, t_min, t_max, t_size)
 		
 	#Plot axes
--- test.py
+++ test.py
@@ -1,29 +1,36 @@
 #Define any number of functions written in python
 def f(x):
-	return 2*x
+	return math.sin(x)
+
+def g(x):
+	return math.cos(x)
+
 
 #Put all the functions you want to plot into this python array
 #Priority is given to later functions
-functions = [f]
+functions = [f,g]
 
 #State whether it should be in polar coordinates
 #(true is polar false is cartesian)
-polar = True
+polar = False
 
 #Define the x size of each character
 #(distance between tick marks on the graph)
-x_size = 1
+x_size = 1/16.
 
 #Define the y size of each character
-y_size = 2
+y_size = 1/10.
 
 #Define the start and end of the x partitions
-x_min = -35
-x_max = 35
+x_min = -89
+x_max = 89
 
 #Define the start and end of the y partitions
-y_min = -17
-y_max = 17
+y_min = 0
+y_max = 0
+
+#Turn autofit on or off
+autofit = True
 
 #State whether or not axes should be plotted
 plot_axes = False
